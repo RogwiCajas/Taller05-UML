70d2bc69df40f74b90c8993489ed953b60e8bb45
walter santacruz <wasantac@espol.edu.ec>
2020-07-09 11:03:05 -0500

    Paciente

--- TallerUML/src/talleruml/Paciente.java
+++ TallerUML/src/talleruml/Paciente.java
@@ -6,6 +6,7 @@
 package talleruml;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *
@@ -16,9 +17,50 @@ public class Paciente extends Persona{
     protected HistoriaClinica historiaClinica;//0 1
     protected Cita cita;//0 1
     protected ArrayList<Receta> recetas;//0 o n
+
+    public Paciente(String usuario, String clave, String nombre, String cedula, String direccion, Date fechaNac,String email,HistoriaClinica historia,
+            Cita cita,ArrayList<Receta> recetas) {
+        super(usuario, clave, nombre, cedula, direccion, fechaNac);
+        this.email = email;
+        this.historiaClinica = historia;
+        this.cita = cita;
+        this.recetas = recetas;
+    }
     
     public boolean solicitarCita(){
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public HistoriaClinica getHistoriaClinica() {
+        return historiaClinica;
+    }
+
+    public void setHistoriaClinica(HistoriaClinica historiaClinica) {
+        this.historiaClinica = historiaClinica;
+    }
+
+    public Cita getCita() {
+        return cita;
+    }
+
+    public void setCita(Cita cita) {
+        this.cita = cita;
+    }
+
+    public ArrayList<Receta> getRecetas() {
+        return recetas;
+    }
+
+    public void setRecetas(ArrayList<Receta> recetas) {
+        this.recetas = recetas;
     }
     
     
